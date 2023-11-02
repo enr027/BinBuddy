434fd67632362c4d8eabc797fbf4838204d0b21e
enr027 <antonio07enrique@gmail.com>
2023-11-02 18:41:33 -0500

    testing from a different computer

--- app/src/main/java/com/example/binbuddy/MainActivity.kt
+++ app/src/main/java/com/example/binbuddy/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("hi")
+        println("testing from a different computer!!!")
     }
 }
