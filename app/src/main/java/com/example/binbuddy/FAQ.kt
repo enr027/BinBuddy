5c2f6bdc1eaff056d17750a53e4e8317e84e4f5d
enriqueantonio <antonio07antonio@gmail.com>
2023-11-16 19:26:02 -0600

    lol

--- app/src/main/java/com/example/binbuddy/FAQ.kt
+++ app/src/main/java/com/example/binbuddy/FAQ.kt
@@ -3,21 +3,12 @@ package com.example.binbuddy
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import com.example.binbuddy.databinding.FragmentUserHomeBinding
 
 
-class UserHome : Fragment(R.layout.fragment_user_home) {
-
-    private lateinit var binding: FragmentUserHomeBinding
+class FAQ : Fragment(R.layout.fragment_f_a_q) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentUserHomeBinding.bind(view)
-
-        binding.userToolBar.inflateMenu(R.menu.appbar)
-
-
     }
 }
