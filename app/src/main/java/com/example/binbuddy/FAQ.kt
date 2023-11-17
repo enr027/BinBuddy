d067cf39db576172818bc8c600daac822184bf5b
enriqueantonio <antonio07antonio@gmail.com>
2023-11-16 20:36:04 -0600

    Navigation between Userhome and FAQ and added Menu bars

--- app/src/main/java/com/example/binbuddy/FAQ.kt
+++ app/src/main/java/com/example/binbuddy/FAQ.kt
@@ -5,10 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.binbuddy.databinding.FragmentFAQBinding
 
 
 class FAQ : Fragment(R.layout.fragment_f_a_q) {
+
+    private lateinit var binding: FragmentFAQBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFAQBinding.bind(view)
+
+        binding.userBackBar.inflateMenu(R.menu.backbar)
+
+        binding.userBackBar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.menuBack ->{
+                    Navigation.findNavController(view).navigate(R.id.FAQBackToUserHome)
+                    true
+                }else -> false
+            }
+        }
+
+
     }
 }
