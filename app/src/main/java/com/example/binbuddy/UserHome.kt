12cb59e1e0493b91bfa77d3b40f64469f605b243
enriqueantonio <antonio07antonio@gmail.com>
2023-11-16 20:25:31 -0600

    lol

--- app/src/main/java/com/example/binbuddy/UserHome.kt
+++ app/src/main/java/com/example/binbuddy/UserHome.kt
@@ -3,9 +3,12 @@ package com.example.binbuddy
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.binbuddy.databinding.FragmentUserHomeBinding
 
 
@@ -16,8 +19,19 @@ class UserHome : Fragment(R.layout.fragment_user_home) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentUserHomeBinding.bind(view)
 
-        binding.userToolBar.inflateMenu(R.menu.appbar)
+        binding.userBar.inflateMenu(R.menu.appbar)
+
+        binding.userBar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.menuFAQ -> {
+                    Navigation.findNavController(view).navigate(R.id.navigateToFAQ)
+                    true
+                }else -> false
+            }
+        }
 
 
     }
+
+
 }
