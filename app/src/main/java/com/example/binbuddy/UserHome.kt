827fb3c1cec00cfcbf99c951f26d91c5f833f4f1
Mulle017 <mulle017@rangers.uwp.edu>
2023-11-18 19:36:39 -0600

    company view, going to add map tomorrow

--- app/src/main/java/com/example/binbuddy/UserHome.kt
+++ app/src/main/java/com/example/binbuddy/UserHome.kt
@@ -10,16 +10,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.binbuddy.databinding.FragmentUserHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 
-class UserHome : Fragment(R.layout.fragment_user_home) {
+class UserHome : Fragment(R.layout.fragment_user_home), OnMapReadyCallback {
+    private var mGoogleMap: GoogleMap? = null
 
     private lateinit var binding: FragmentUserHomeBinding
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+            val mapFragment = childFragmentManager
+                .findFragmentById(R.id.mapFragment2) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+
             binding = FragmentUserHomeBinding.bind(view)
             binding.imageButton8.setOnClickListener {
                 Navigation.findNavController(view).navigate(R.id.navigateToFAQ)
+
+
             }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 }
