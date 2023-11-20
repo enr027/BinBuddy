72c0eb122ec11bc94798e2a10a7e9cfd6beeb0cf
Mulle017 <mulle017@rangers.uwp.edu>
2023-11-19 22:07:54 -0600

    added login/create account

--- app/src/main/java/com/example/binbuddy/CreateAccountFragment.kt
+++ app/src/main/java/com/example/binbuddy/CreateAccountFragment.kt
@@ -1,36 +1,38 @@
 package com.example.binbuddy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.binbuddy.databinding.FragmentCreateAccountBinding
-import com.example.binbuddy.databinding.FragmentLoginBinding
-import java.util.NavigableMap
-
+import com.google.firebase.auth.FirebaseAuth
 
 class CreateAccountFragment : Fragment(R.layout.fragment_create_account) {
 
     private lateinit var binding: FragmentCreateAccountBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCreateAccountBinding.bind(view)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.butNewAcc.setOnClickListener {
+            val email = binding.EtEmail.text.toString()
+            val password = binding.EtPassWord.text.toString()
+
             if (binding.EtEmail.text.isNotBlank() && binding.EtPassWord.text.isNotBlank() &&
                 binding.EtEnterFirstName.text.isNotBlank() && binding.EtEnterLastName.text.isNotBlank()
                 && binding.EtEnterDirection.text.isNotBlank() && binding.EtNumberOfBins.text.isNotBlank()) {
-                Navigation.findNavController(view).navigate(R.id.navigateBackToLogin)
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful) {
+                        Navigation.findNavController(view).navigate(R.id.navigateBackToLogin)
+                    }
+                }
             }else{
                 Toast.makeText(context,"Please fill out all fields",Toast.LENGTH_LONG).show()
             }
         }
-
     }
-
-
 }
--- app/src/main/java/com/example/binbuddy/loginFragment.kt
+++ app/src/main/java/com/example/binbuddy/loginFragment.kt
@@ -1,30 +1,45 @@
 package com.example.binbuddy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.binbuddy.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class loginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
 
-        binding.butCreateNewAcc.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.navigateToCreateAcc)
-        }
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.butUserLogin.setOnClickListener {
-            if (binding.etLoginEmail.text.isNotBlank() && binding.etLoginPassword.text.isNotBlank()){
-                Navigation.findNavController(view).navigate(R.id.navigateToUserHome)
-            }else{
-                Toast.makeText(context,"Please fill out all fields", Toast.LENGTH_LONG).show()
+            val email = binding.etLoginEmail.text.toString()
+            val password = binding.etLoginPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Navigation.findNavController(view).navigate(R.id.navigateToUserHome)
+                    }
+                    else
+                    {
+                        Toast.makeText(context,"Please fill out all fields", Toast.LENGTH_LONG).show()
+                    }
+                }
             }
+        }
+
+        binding.butCreateNewAcc.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.navigateToCreateAcc)
         }
 
         binding.butCompanyLogin.setOnClickListener {
