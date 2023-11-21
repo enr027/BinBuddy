d4150e9a00fc2d6cd3e9be1d8474b200555d5950
Mulle017 <mulle017@rangers.uwp.edu>
2023-11-20 20:16:31 -0600

    added login/create account

---
+++ app/src/main/java/com/example/binbuddy/JobData.kt
@@ -0,0 +1,6 @@
+package com.example.binbuddy
+
+data class JobData(val trash : String? = null,
+                   val amount : String? = null,
+                   val date : String? = null,
+                   val address : String? = null)
--- app/src/main/java/com/example/binbuddy/UserHome.kt
+++ app/src/main/java/com/example/binbuddy/UserHome.kt
@@ -15,48 +15,60 @@ import com.google.firebase.database.FirebaseDatabase
 class UserHome : Fragment(R.layout.fragment_user_home), OnMapReadyCallback {
     private var mGoogleMap: GoogleMap? = null
 
-    private lateinit var binding: FragmentUserHomeBinding
     private lateinit var database : DatabaseReference
+    private lateinit var binding: FragmentUserHomeBinding
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+            super.onViewCreated(view, savedInstanceState)
 
             val mapFragment = childFragmentManager
                 .findFragmentById(R.id.mapFragment2) as SupportMapFragment
             mapFragment.getMapAsync(this)
 
-            val database = FirebaseDatabase.getInstance()
-            val dataRef = database.getReference("Users")
-
-
-
-
+            binding = FragmentUserHomeBinding.bind(view)
 
             binding.addJob.setOnClickListener {
+
+                database = FirebaseDatabase.getInstance().getReference("Jobs")
+
                 val trashType = binding.etTrash.text.toString()
                 val amount = binding.etAmount.text.toString()
                 val datePickUp = binding.etDate.text.toString()
                 val address = binding.etAddress.text.toString()
+
+                if (binding.etTrash.text != null && binding.etAmount.text != null
+                    && binding.etDate.text != null && binding.etAddress.text != null
+                ) {
+
+                    val JobData = JobData(trashType, amount, datePickUp, address)
+
+                    database.child(address).setValue(JobData).addOnSuccessListener {
+
+                        //Clear the fields
+                        binding.etTrash.text!!.clear()
+                        binding.etAmount.text!!.clear()
+                        binding.etDate.text!!.clear()
+                        binding.etAddress.text!!.clear()
+                    }
+                }
             }
 
-            binding = FragmentUserHomeBinding.bind(view)
-            binding.imageButton8.setOnClickListener {
-                Navigation.findNavController(view).navigate(R.id.navigateToFAQ)
-            }
+                binding.imageButton8.setOnClickListener {
+                    Navigation.findNavController(view).navigate(R.id.navigateToFAQ)
+                }
 
-            binding.imageButton7.setOnClickListener {
-                Navigation.findNavController(view).navigate(R.id.navigateToCalendarUser)
-            }
+                binding.imageButton7.setOnClickListener {
+                    Navigation.findNavController(view).navigate(R.id.navigateToCalendarUser)
+                }
 
-            binding.imageButton6.setOnClickListener {
-                Navigation.findNavController(view).navigate(R.id.navigateToNotificationsUser)
-            }
+                binding.imageButton6.setOnClickListener {
+                    Navigation.findNavController(view).navigate(R.id.navigateToNotificationsUser)
+                }
 
-            binding.imageButton.setOnClickListener {
-                Navigation.findNavController(view).navigate(R.id.navigateToUserAccountMenu)
+                binding.imageButton.setOnClickListener {
+                    Navigation.findNavController(view).navigate(R.id.navigateToUserAccountMenu)
+                }
             }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mGoogleMap = googleMap
-    }
-}
+            override fun onMapReady(googleMap: GoogleMap) {
+                mGoogleMap = googleMap
+            }
+        }
