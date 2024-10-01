9f30af8417953b882f9d9e0c27cc76f3c4676de9
enriqueantonio <antonio07antonio@gmail.com>
2024-10-01 09:55:56 -0500

    Kotlin comments

--- app/src/main/java/com/example/binbuddy/JobData.kt
+++ app/src/main/java/com/example/binbuddy/JobData.kt
@@ -7,3 +7,4 @@ data class JobData(val trash : String? = null,
                    val amount : String? = null,
                    val date : String? = null,
                    val address : String? = null)
+
